a9a564bfa39001ed5a36488e3cd70918d2906d30
Thiago Fassina <thiagoaaa@gmail.com>
2017-02-01 10:49:50 -0200

    converted most things to stateless componenents

--- app/src/index.js
+++ app/src/index.js
@@ -1,63 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class ParameterFlag extends React.Component {
-    render() {
-        return (
-            <span className="badge badge-default">${this.props.flagId}</span>
-        );
-    }
-}
 
-class ParameterName extends React.Component {
-    render() {
-        return (
-            <input type="text" className="form-control form-control-sm" />
-        );
-    }
-}
+const ParameterFlag = ({flagText}) => (
+    <span className="badge badge-default">
+        ${flagText}
+    </span>
+);
 
-class ParameterOptions extends React.Component {
-    render() {
-        return (
-            <i className="fa fa-chevron-down" />
-        );
-    }
-}
+const ParameterName = () => (
+    <input type="text" className="form-control form-control-sm" />
+);
 
-class ParametersTableRow extends React.Component {
-    render() {
-        return (
-            <tr>
-                <td className="text-right">
-                    <ParameterFlag flagId={this.props.flagId} />
-                </td>
-                <td>
-                    <ParameterName />
-                </td>
-                <td className="text-center">
-                    <ParameterOptions />
-                </td>
-            </tr>
-        );
-    }
-}
+const ParameterOptions = () => <i className="fa fa-chevron-down" />;
 
-class ParametersTableFooter extends React.Component {
-    render() {
-        return (
-            <tr>
-                <td colSpan="3">
-                    <button className="btn btn-secondary btn-block">Add</button>
-                </td>
-            </tr>
-        );
-    }
-}
+const ParametersTableRow = ({flagText}) => (
+    <tr>
+        <td className="text-right">
+            <ParameterFlag flagText={flagText} />
+        </td>
+        <td>
+            <ParameterName />
+        </td>
+        <td className="text-center">
+            <ParameterOptions />
+        </td>
+    </tr>
+);
+
+const ParametersTableFooter = () => (
+    <tr>
+        <td colSpan="3">
+            <button className="btn btn-secondary btn-block">Add</button>
+        </td>
+    </tr>
+);
 
 class ParametersTable extends React.Component {
     tableRows() {
-        let mapper = (i) => <ParametersTableRow key={i} flagId={i} />;
+        let mapper = (text, i) => <ParametersTableRow key={i} flagText={text} />;
         return this.props.parameters.map(mapper);
     }
 
@@ -73,124 +54,87 @@ class ParametersTable extends React.Component {
     }
 }
 
-class Parameters extends React.Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col">
-                    <h1>Choose parameters</h1>
-                    <ParametersTable parameters={['test']} />
-                </div>
-            </div>
-        );
-    }
-}
+const Parameters = () => (
+    <div className="row">
+        <div className="col">
+            <h1>Choose parameters</h1>
+            <ParametersTable parameters={['test', 'row2']} />
+        </div>
+    </div>
+);
 
 
-class FormulaInput extends React.Component {
-    render() {
-        return (
-            <input type="text" className="form-control" />
-        );
-    }
-}
+const FormulaInput = () => <input type="text" className="form-control" />;
 
-class FormulaPreview extends React.Component {
-    render() {
-        return (
-            <div className="card">
-                <div className="card-block text-center text-muted">
-                    Human-readable version here
-                </div>
-            </div>
-        );
-    }
-}
+const FormulaPreview = () => (
+    <div className="card">
+        <div className="card-block text-center text-muted">
+            Human-readable version here
+        </div>
+    </div>
+);
 
-class Formula extends React.Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col">
-                    <h1>Write formula</h1>
-                    <FormulaInput />
-                    <FormulaPreview />
-                </div>
-            </div>
-        );
-    }
-}
+const Formula = () => (
+    <div className="row">
+        <div className="col">
+            <h1>Write formula</h1>
+            <FormulaInput />
+            <FormulaPreview />
+        </div>
+    </div>
+);
 
 
-class EvaluationInputs extends React.Component {
-    render() {
-        return (
-            <div className="text-right">
-                <pre>X</pre>
-                <input type="text" className="form-control form-control-sm" />
-            </div>
-        );
-    }
-}
+const EvaluationInputs = () => (
+    <div className="text-right">
+        <pre>X</pre>
+        <input type="text" className="form-control form-control-sm" />
+    </div>
+);
 
-class EvaluationResult extends React.Component {
-    render() {
-        return (
-            <input
-                type="text"
-                className="form-control form-control sm"
-                disabled
-                value="666"
-            />
-        );
-    }
-}
+const EvaluationResult = () => (
+    <input
+        type="text"
+        className="form-control form-control sm"
+        disabled
+        value="666"
+    />
+);
 
-class EvaluationTest extends React.Component {
-    render() {
-        return (
-            <div className="row align-items-center">
-                <div className="col-5">
-                    <EvaluationInputs />
-                </div>
-                <div className="col-2 text-center">
-                    <i className="fa fa-arrow-right fa-2x"></i>
-                </div>
-                <div className="col-5">
-                    <EvaluationResult />
-                </div>
-            </div>
-        );
-    }
-}
+const EvaluationTest = () => (
+    <div className="row align-items-center">
+        <div className="col-5">
+            <EvaluationInputs />
+        </div>
+        <div className="col-2 text-center">
+            <i className="fa fa-arrow-right fa-2x"></i>
+        </div>
+        <div className="col-5">
+            <EvaluationResult />
+        </div>
+    </div>
+);
 
-class Evaluation extends React.Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col">
-                    <h1>Evaluate</h1>
-                    <EvaluationTest />
-                </div>
-            </div>
-        );
-    }
-}
+const Evaluation = () => (
+    <div className="row">
+        <div className="col">
+            <h1>Evaluate</h1>
+            <EvaluationTest />
+        </div>
+    </div>
+);
 
 
-class CreateFormula extends React.Component {
-    render() {
-        return (
-            <div>
-                <Parameters />
-                <hr />
-                <Formula />
-                <hr />
-                <Evaluation />
-            </div>
-        );
-    }
-}
+const CreateFormula = () => (
+    <div>
+        <Parameters />
+        <hr />
+        <Formula />
+        <hr />
+        <Evaluation />
+    </div>
+);
+
 
 ReactDOM.render(
     <CreateFormula />,
