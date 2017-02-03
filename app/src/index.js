81aa650d6c17e2e341f8b6c5cefc0d1d7c71db28
Thiago Fassina <thiagoaaa@gmail.com>
2017-02-03 16:47:29 -0200

    extracted App abstraction

--- app/src/index.js
+++ app/src/index.js
@@ -1,53 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Parameters from './parameters.jsx';
-import Formula from './formula.jsx';
-import Evaluation from './evaluation.jsx';
-
-const createInput = (label) => ({label: label, value: ''});
-
-class CreateFormula extends React.Component {
-    constructor() {
-        super();
-        this.addParameter = this.addParameter.bind(this);
-        this.updateValue = this.updateValue.bind(this);
-
-        this.state = {inputList: [createInput('natural')]};
-    }
-
-    addParameter() {
-        let newList = this.state.inputList.concat([createInput('artificial')]);
-        this.setState({inputList: newList});
-    }
-
-    updateValue(i, value) {
-        var inputs = this.state.inputList.slice();
-        inputs[i].value = value;
-        this.setState({inputList: inputs});
-    }
-
-    render() {
-        return (
-            <div>
-                <Parameters
-                    list={this.state.inputList}
-                    addHandler={this.addParameter}
-                />
-                <hr />
-                <Formula />
-                <hr />
-                <Evaluation
-                    inputs={this.state.inputList}
-                    updateHandler={this.updateValue}
-                />
-            </div>
-        );
-    }
-}
-
+import App from './app.js';
 
 ReactDOM.render(
-    <CreateFormula />,
+    <App />,
     document.getElementById('root')
 );
