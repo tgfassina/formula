e60808d600159d628ec5cdb63d23e05711cb901c
Thiago Fassina <thiagoaaa@gmail.com>
2017-02-09 11:10:33 -0200

    adjusted grid for all display sizes

--- app/src/components/parts/app-section.js
+++ app/src/components/parts/app-section.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AppSection = ({children}) => (
     <div className="row justify-content-center">
-        <div className="col-12">
+        <div className="col-sm-12 col-md-6 col-lg-5 col-xl-4">
             {children}
         </div>
     </div>
