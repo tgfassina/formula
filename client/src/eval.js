6891c7f96071d94ede6f3d70b36212216c9a3d95
Thiago Fassina <thiagoaaa@gmail.com>
2017-03-03 13:39:58 -0300

    introduced react router

---
+++ client/src/eval.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import './vendor/index.js'
+import './styles/custom.css';
+import './styles/formula.css';
+
+import Parameters from './components/parameters.js';
+import Formula from './components/formula.js';
+import Evaluation from './components/evaluation.js';
+
+import AppModel from './models/app.js';
+
+class Eval extends React.Component {
+    constructor() {
+        super();
+        this.model = new AppModel(this.setState.bind(this));
+        this.state = this.model.export();
+    }
+
+    save() {
+        fetch('/echo', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.model.exportForDatabase())
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Parameters
+                    parameters={this.state.parameters}
+                    onAdd={this.model.getParametersAdder()}
+                    updater={this.model.getParametersUpdater()}
+                    deleter={this.model.getParametersDeleter()}
+                />
+                <Formula
+                    placeholder={this.state.formulaPlaceholder}
+                    onUpdate={this.model.getFormulaUpdater()}
+                />
+                <Evaluation
+                    parameters={this.state.parameters}
+                    result={this.state.result}
+                    updater={this.model.getValuesUpdater()}
+                />
+                <hr />
+                <button className="btn btn-secondary" onClick={this.save.bind(this)}>Save</button>
+            </div>
+        );
+    }
+}
+
+export default Eval;
