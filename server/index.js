662f28599154beadce61f0f2811a9f244a6fd9db
Thiago Fassina <thiagoaaa@gmail.com>
2017-03-03 18:46:20 -0300

    server: added "save" endpoint that tries to store data on local mongodb

--- server/index.js
+++ server/index.js
@@ -1,14 +1,40 @@
 const express = require('express')
 const server = express()
 
-const bodyParser = require('body-parser');
-server.use(bodyParser.json());
+const bodyParser = require('body-parser')
+server.use(bodyParser.json())
 
-server.post('/echo', function(req, res) {
-    console.log(req.body)
-    res.json({echo: req.body})
+var mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/test').then(function(a, b) {
+    console.log('MongoDB: connected')
+}, function(err) {
+    console.log('MongoDB: connection error', err)
+})
+
+var formulaSchema = mongoose.Schema({
+    title: String,
+    parameters:[
+        {
+            variable: String,
+            defaultValue: String,
+            label: String,
+        }
+    ],
+    formula: String,
+})
+var Formula = mongoose.model('Formula', formulaSchema)
+
+
+
+server.post('/save', function(req, res) {
+    formula = new Formula(req.body)
+    formula.save().then(function(savedObj) {
+        console.log("Save successful. ",savedObj.id)
+        res.json({id: savedObj.id})
+    })
 })
 
 server.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('Server listening on port 3000')
 })
