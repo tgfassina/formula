5911b7f04e671b4d3d8dc351ea9a70d558e2cc40
Thiago Fassina <thiagoaaa@gmail.com>
2017-03-15 16:51:12 -0300

    added loading state to eval

--- app/src/eval.js
+++ app/src/eval.js
@@ -6,6 +6,28 @@ import FormulaDisplay from './components/formula-display.js'
 
 import AppModel from './models/app.js'
 
+const EmptyTitle = () => (
+    <em className="text-muted">Untitled</em>
+)
+
+const EvalData = ({title, formula, parameters, result, updater}) => (
+    <div>
+        <h1>{title || <EmptyTitle />}</h1>
+        <FormulaDisplay formula={formula} />
+        <EvaluationTester
+            parameters={parameters}
+            result={result}
+            updater={updater}
+        />
+    </div>
+)
+
+const EvalLoading = () => (
+    <h2 className="text-center">
+        <i className="fa fa-spinner fa-spin" />
+    </h2>
+)
+
 class Eval extends React.Component {
     constructor(props) {
         super(props)
@@ -16,6 +38,7 @@ class Eval extends React.Component {
     }
 
     componentDidMount() {
+        this.setState({loaded: false})
         this.load()
     }
 
@@ -26,24 +49,21 @@ class Eval extends React.Component {
             .child(this.props.params.key)
             .once('value')
             .then(snap => this.model.import(snap.val().data))
+            .then(() => this.setState({loaded: true}))
     }
 
     render() {
         return (
             <div>
-                <h1>
-                    {
-                        this.model.title
-                        ||
-                        <em className="text-muted">Untitled</em>
-                    }
-                </h1>
-                <FormulaDisplay formula={this.state.formula} />
-                <EvaluationTester
-                    parameters={this.state.parameters}
-                    result={this.state.result}
-                    updater={this.model.getValuesUpdater()}
-                />
+                {this.state.loaded ?
+                    <EvalData
+                        title={this.model.title}
+                        formula={this.state.formula}
+                        parameters={this.state.parameters}
+                        result={this.state.result}
+                        updater={this.model.getValuesUpdater()}
+                    />
+                : <EvalLoading />}
             </div>
         )
     }
