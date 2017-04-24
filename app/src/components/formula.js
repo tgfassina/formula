a4b244a94ee8b77528cf5dcf6aec7d08c8fc1bfb
Josmar Dias <josmarnet@gmail.com>
2017-04-23 23:33:11 -0300

    <FormulaInput /> doesn't need state

--- app/src/components/formula.js
+++ app/src/components/formula.js
@@ -2,22 +2,17 @@ import React from 'react'
 
 import AppSection from './parts/app-section.js'
 
-class FormulaInput extends React.Component {
-    handleChange(event) {
-        this.props.onUpdate(event.target.value)
-    }
-
-    render() {
-        return (
-            <input
-                type="text"
-                className="form-control formula-input"
-                placeholder={this.props.placeholder}
-                onChange={this.handleChange.bind(this)}
-            />
-        )
-    }
-}
+const FormulaInput = ({
+    placeholder,
+    onUpdate,
+}) => (
+    <input
+        type="text"
+        className="form-control formula-input"
+        placeholder={placeholder}
+        onChange={(event) => onUpdate(event.target.value)}
+    />
+)
 
 const Formula = ({placeholder, onUpdate}) => (
     <AppSection>
